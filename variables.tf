58d9f731f51798292c8844aa9bff6d9b5127cdef
sfell77 <github@sfproductions.net>
2021-06-25 15:36:12 -0400

    update template

--- variables.tf
+++ variables.tf
@@ -10,30 +10,26 @@ var "app_name" {
 
 var "tag_owner_contact" {
     description = "Email address/idnetifier of the TEAM responsible for this app"
+    default = "owner-contact-info-here"
 }
 
-# Variables you'll pass at run time -- you can leave these blank unless they're region agnostic
-var "ami_id" {
-    description = "The AMI ID you'll use for your EC2 (if you're using one).  We don't use 'latest' since that doesn't stay the same, and we potentially haven't validated that it works with our stack"
-    default = ""
+var "anything_else" {
+    description = "Any variables in a module you're leveraging that aren't included below that'll get passed @ run time"
 }
 
+# Variables you'll pass @ run time -- you can leave these blank unless they're region agnostic
 var "aws_region" {
     description = "The AWS region you're deploying to.  For example, us-east-1"
-    default = ""
 }
 
 var "deploy_env" {
     description = "Denotes dev/qa/prod regions (or whatever values you use); needed for map variables"
-    default = ""
 }
 
 var "s3_bucket_name" {
     description = "We save statefiles to S3 so that they're in a centralized location that's not your laptop. Remember to create buckets PER REGION or you may not have access to your statefiles in the event of a regional outage"
-    default = ""
 }
 
 var "tag_deployment_owner" {
     description = "Email address/identifier of the PERSON deploying this app"
-    default = ""
 }
